81f2f7a6f45fb03f2e83829db62c638a3a46742e
Vénus <boursautvenus@gmail.com>
2022-10-20 13:56:08 +0200

    j'ai fait le css de la nav bar

--- webfiles/js/script.js
+++ webfiles/js/script.js
@@ -13,4 +13,49 @@ fetch("https://api.open-meteo.com/v1/forecast?latitude=50.49&longitude=2.55&hour
 }).finally(() => {
     console.log("Experiment completed")
 })
+
+
+
+//menu nav 
+
+let meteoMenu = document.createElement("div");
+let apparitionLi = document.querySelector("ul li:last-of-type");
+let htmlMenu = document.querySelector("menu");
+
+
+meteoMenu.innerHTML="<span></span>";
+meteoMenu.classList.add("meteo-menu");
+
+window.addEventListener("resize", () => {
+
+    meteoDisplay();
+    if (window.innerWidth > 768) {
+        meteoMenu.remove();
+    }
+})
+
+window.addEventListener("load", () => {
+    meteoDisplay();
+})
+
+
+/**
+ * Affiche le burger menu a partir du moment ou on atteint une resolution inferrieur a 768 px
+ * @author Vénus Boursaut
+ * @version 1.0
+ */
+function meteoDisplay() {
+    if (window.innerWidth <= 768 && !apparitionLi.contains(meteoMenu)) {
+        apparitionLi.prepend(meteoMenu);
+    }
+}
+
+
+meteoMenu.addEventListener("click", () =>{
+    meteoMenu.classList.toggle("active");
+    htmlMenu.classList.toggle("active");
+    console.log("cliker")
+})
+
+
 //})
