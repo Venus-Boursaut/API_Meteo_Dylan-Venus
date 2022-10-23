6b8b03a81d1840a90c0eb989a7a6e769a762513b
ShuShogekiha <105912763+ShuShogekiha@users.noreply.github.com>
2022-10-23 20:54:37 +0200

    amélioration js

--- webfiles/js/script.js
+++ webfiles/js/script.js
@@ -9,28 +9,50 @@ const options = {
 };
 
 let saisi = document.getElementsByClassName("saisie")[0];
+let temp = document.getElementsByClassName("temp")[0];
+let date = document.getElementsByClassName("date")[0];
+
 
 saisi.addEventListener("keypress", (e) => {
     if (e.code === "Enter") {
 
-        fetch(`https://spott.p.rapidapi.com/places/autocomplete?limit=10&skip=0&language=%20fr&country=FR&q=${saisi.value}&type=CITY`, options)
+        fetch(`https://spott.p.rapidapi.com/places/autocomplete?limit=10&skip=0&language=%20fr&q=${saisi.value}&type=CITY`, options)
             .then(response => response.json())
             .then((response) => {
                 let position = response[0].coordinates;
                 console.log(position);
                 apiMetoFrance(position);
             })
-            .then(response => console.log(response))
+            .catch(err => {
+                const filtre = new RegExp('coordinates', 'i');
+
+                if (filtre.test(err)){
+                    console.log("mauvaise syntax");
+                    let error = document.getElementsByClassName("erreur-ville")[0];
+                    let erreurH2 = document.createElement("h2");
+                    erreurH2.textContent = "mauvaise syntaxe";
+                    erreurH2.setAttribute("class", "erreur-lu");
+                    error.appendChild(erreurH2);
+                } else console.error(err);
+
+            })
+            .finally(() => console.log("experiment finish"));
     }
 })
 
 function apiMetoFrance(val) {
-    fetch(`https://api.open-meteo.com/v1/meteofrance?latitude=${val.latitude}&longitude=${val.longitude}&hourly=temperature_2m&current_weather=true&timezone=Europe%2FBerlin&past_days=7`)
+    fetch(`https://api.open-meteo.com/v1/meteofrance?latitude=${val.latitude}&longitude=${val.longitude}&hourly=temperature_2m&current_weather=true&past_days=7&timezone=auto`)
         .then(response => response.json())
         .then(response => {
-            console.log(response)
+            if ( document.getElementsByClassName("erreur-ville")[0].childElementCount !== 0){
+                document.getElementsByClassName("erreur-lu")[0].remove();
+            }
+            console.log(response);
+            temp.textContent = response.current_weather.temperature + response.hourly_units.temperature_2m;
+            date.textContent = response.current_weather.time;
         })
-        .catch(err => console.error(err));
+        .catch(err => console.error(err))
+        .finally(() => console.log("experiment finish"));
 }
 
 
